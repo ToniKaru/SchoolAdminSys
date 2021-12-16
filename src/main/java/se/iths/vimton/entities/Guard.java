946de209ccb7389c0c2380fad70f8a9aa14d5df4
Vimbayinashe <vimbayinashemandaza@gmail.com>
2021-12-16 15:32:45 +0100

    Fixes Guard.Against.ssnInvalid
    - added hyphens to ssn becauase dateInvalid checks for only 10 characters
      first

--- src/main/java/se/iths/vimton/entities/Guard.java
+++ src/main/java/se/iths/vimton/entities/Guard.java
@@ -32,13 +32,14 @@ public class Guard {
 
         public static void ssnInvalid(String ssn) {
             if (ssn.length() != 13 && !ssn.matches("\\d{8}-\\d{4}"))
-                throw new IllegalArgumentException();
-            dateInvalid(ssn.substring(0,8));
+                throw new IllegalArgumentException(ssn + " is an invalid ssn.");
+            String date = ssn.substring(0,4) + "-" + ssn.substring(4, 6) + "-" + ssn.substring(6, 8);
+            dateInvalid(date);
         }
 
         public static void dateInvalid(String date){
-            if (date.length() != 10 && (!date.matches("\\d{4}-\\d{2}-\\d{2}") ||  !date.matches("\\d{8}")))
-                throw new IllegalArgumentException();
+            if (date.length() != 10 && !date.matches("\\d{4}-\\d{2}-\\d{2}"))
+                throw new IllegalArgumentException(date + " is an invalid date.");
             LocalDate localDate = LocalDate.parse(date);
             yearOutsideValidRange(localDate.getYear());
         }
