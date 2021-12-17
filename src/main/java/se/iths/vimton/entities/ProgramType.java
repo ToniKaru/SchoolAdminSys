fff167558d17e670a02cbfd3de84bf2104943573
Vimbayinashe <vimbayinashemandaza@gmail.com>
2021-12-17 11:09:01 +0100

    Refactor equals & hashcode in ProgramType & StudyLevel

--- src/main/java/se/iths/vimton/entities/ProgramType.java
+++ src/main/java/se/iths/vimton/entities/ProgramType.java
@@ -77,13 +77,13 @@ public class ProgramType {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         ProgramType that = (ProgramType) o;
-        return id == that.id && creditsRequired == that.creditsRequired && accredited == that.accredited &&
-               Objects.equals(studyLevel, that.studyLevel) && Objects.equals(programs, that.programs);
+        return creditsRequired == that.creditsRequired && accredited == that.accredited &&
+               Objects.equals(studyLevel, that.studyLevel);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, creditsRequired, accredited, studyLevel, programs);
+        return Objects.hash(creditsRequired, accredited, studyLevel);
     }
 
     @Override
