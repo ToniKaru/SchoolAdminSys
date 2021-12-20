d831be80bad06fbe2b7d1e88651299b071e24604
Vimbayinashe <vimbayinashemandaza@gmail.com>
2021-12-20 10:16:32 +0100

    Add updateTeacher & showDetails methods

--- src/main/java/se/iths/vimton/menus/TeacherMenu.java
+++ src/main/java/se/iths/vimton/menus/TeacherMenu.java
@@ -7,13 +7,19 @@ import se.iths.vimton.impl.TeacherDaoImpl;
 
 import javax.persistence.EntityManagerFactory;
 import java.util.List;
+import java.util.Optional;
+
+import static se.iths.vimton.Menu.getUserInput;
+import static se.iths.vimton.Menu.scanner;
 
 public class TeacherMenu {
 
     TeacherDao teacherDao;
+    List<Teacher> teachers;
 
     public TeacherMenu(EntityManagerFactory emf) {
         this.teacherDao = new TeacherDaoImpl(emf);
+        teachers = teacherDao.getAll();
     }
 
 
@@ -46,11 +52,60 @@ public class TeacherMenu {
             case 0 -> Menu.cancel();
 //            case 1 -> addTeacher();
             case 2 -> showAll();
-//            case 3 -> updateCourse();
-//            case 4 -> showCourseDetails();
+            case 3 -> updateTeacher();
+            case 4 -> showTeacher();
 //            case 5 -> deleteCourse();
             default -> System.out.println("Invalid choice");
         }
+    }
+
+    private void showTeacher() {
+        int id = getUserInput("teacher id", teachers.get(0).getId(), teachers.get(teachers.size() - 1).getId());
+        Optional<Teacher> teacher = teacherDao.getById(id);
+
+        teacher.ifPresentOrElse(
+                System.out::println,
+                () -> System.out.println("Teacher id " + id + " not found.")
+        );
+    }
+
+    private void updateTeacher() {
+        int id = getUserInput("teacher id", teachers.get(0).getId(), teachers.get(teachers.size() - 1).getId());
+        Optional<Teacher> teacher = teacherDao.getById(id);
+
+        if(teacher.isEmpty()) {
+            System.out.println("Teacher id " + id + " not found.");
+            return;
+        }
+
+        System.out.println("Enter teacher's new firstname or 'x' to skip:");
+        String firstName = scanner.nextLine().trim();
+
+        System.out.println("Enter teacher's new lastname or 'x' to skip:");
+        String lastName = scanner.nextLine().trim();
+
+        System.out.println("Enter teacher's new phone number or 'x' to skip:");
+        String phoneNumber = scanner.nextLine().trim();
+
+        System.out.println("Enter teacher's new email address or 'x' to skip:");
+        String email = scanner.nextLine().trim();
+
+        if (!firstName.equalsIgnoreCase("x"))
+            teacher.get().setFirstName(firstName);
+        if (!lastName.equalsIgnoreCase("x"))
+            teacher.get().setLastName(firstName);
+        if (!phoneNumber.equalsIgnoreCase("x"))
+            teacher.get().setPhoneNumber(firstName);
+        if (!email.equalsIgnoreCase("x"))
+            teacher.get().setEmail(firstName);
+
+        teacherDao.update(teacher.get());
+
+        refreshTeachers();
+    }
+
+    private void refreshTeachers() {
+        teachers = teacherDao.getAll();
     }
 
     private void showAll() {
