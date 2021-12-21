755a4a5a81ffe6b186ae4f80705756a9100116b8
ToniKaru <toni.karunaratne@iths.se>
2021-12-21 10:16:43 +0100

    Added beginnings of StudentMenu

---
+++ src/main/java/se/iths/vimton/menus/StudentMenu.java
@@ -0,0 +1,43 @@
+package se.iths.vimton.menus;
+
+import se.iths.vimton.Menu;
+import se.iths.vimton.dao.StudentDao;
+import se.iths.vimton.entities.Course;
+import se.iths.vimton.entities.Student;
+import se.iths.vimton.impl.StudentDaoImpl;
+
+import javax.persistence.EntityManagerFactory;
+import java.util.List;
+import java.util.Optional;
+
+import static se.iths.vimton.Menu.getUserInput;
+
+public class StudentMenu {
+
+    StudentDao studentDao;
+    List<Student> students;
+
+    public StudentMenu(EntityManagerFactory emf) {
+        this.studentDao = new StudentDaoImpl(emf);
+        this.students = studentDao.getAll();
+    }
+
+    public Optional<Student> getExistingStudentFromUser() {
+        int id = getUserInput("student id", 1, students.size());
+        Optional<Student> student = studentDao.getById(id);
+
+        if (student.isEmpty()) {
+            System.out.println("Course id " + id + " not found.");
+            return null;
+        }
+        return student;
+    }
+
+    private void refreshStudents() {
+        students = studentDao.getAll();
+    }
+
+    private void showAll() {
+       //printStudentSummary(studentDao.getAll())
+    }
+}
